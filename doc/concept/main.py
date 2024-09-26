ffd47fe21987617d643103c3ed5b764070f7cbcc
kazanskii.da@phystech.edu <kazanskii.da@phystech.edu>
2024-09-26 19:38:07 +0300

    init .config params

--- doc/concept/main.py
+++ doc/concept/main.py
@@ -1,5 +1,14 @@
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher #, executor, types
+import yaml, os
 
-bot = Bot(token='')
+file_path = '.config/fin_bot_config.yaml'
+if not os.path.exists(file_path):
+    print (os.getcwd())
+    print(f'file not found')
+with open(file_path,'rt') as config_file:
+    config = yaml.safe_load(config_file)
+token = config['token']
+print(token)
+#bot = Bot(token='')
 
-dp = Dispatcher(bot)
+#dp = Dispatcher(bot)
