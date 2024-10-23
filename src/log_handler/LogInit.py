76c35f2ed60dc128b3230c41fee9146e669f292b
kazanskii.da@phystech.edu <kazanskii.da@phystech.edu>
2024-10-23 19:25:30 +0300

    fix cyr chars in logging

--- src/log_handler/LogInit.py
+++ src/log_handler/LogInit.py
@@ -1,5 +1,7 @@
 import logging
+import sys
 
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s') 
+sys.stdout.reconfigure(encoding='utf-8')
 logger = logging.getLogger(__name__)
